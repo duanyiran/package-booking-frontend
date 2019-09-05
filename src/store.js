608ae3fb47ca83c86cb576a373d188bb5be536c5
duanyiran <dyr6@qq.com>
2019-09-05 18:00:50 +0800

    day15

--- src/store.js
+++ src/store.js
@@ -1,16 +1,44 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
+Vue.use(axios)
 
 export default new Vuex.Store({
   state: {
+    packageList: []
+
+  },
+  getters: {
+    getList: function (state) {
+      return state.packageList;
+    }
 
   },
   mutations: {
+    initPackage: function (state, pageList) {
+      state.packageList = pageList;
+    }
 
   },
   actions: {
+    addPackage(context) {
+      let url = "localhost:10010/employees";
+      axios
+        .post(url)
+        .then(function (response) {
+          context.dispatch("getPackage")
+        })
+    },
+    getPackage(context) {
+      let url = "localhost:10010/employees";
+      axios
+        .get(url)
+        .then(function (response) {
+          context.commit("initPackage", response.data)
+        })
+    },
+
 
   }
 })
---
+++ src/views/PackageList.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-list itemLayout="horizontal" :dataSource="PackageList">
+    <a-list-item slot="renderItem" slot-scope="item, index">
+      <a-list-item-meta>
+        <a slot="title">{{item.username}}</a>
+      </a-list-item-meta>
+    </a-list-item>
+  </a-list>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      PackageList: []
+    };
+  },
+  created: function() {
+    PackageList = this.$store.state.getters.getList;
+  },
+  computed: function() {}
+};
+</script>
+<style>
+</style>
